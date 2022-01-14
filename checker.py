4f7467349e014b84e089616cf3942c92083d483a
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2022-01-14 16:02:47 +0000

    Add reporting of certificate algorithm

--- checker.py
+++ checker.py
@@ -28,7 +28,7 @@ def check_urls():
 def save_results():
 	with open('output.csv', 'w', newline='', encoding='utf-8') as f:
 		write = csv.writer(f)
-		write.writerow(['host', 'port', 'before', 'after', 'result'])
+		write.writerow(['host', 'port', 'algorithm', 'before', 'after', 'result'])
 		write.writerows(results)
 
 
@@ -42,21 +42,21 @@ def process_host(string):
 	port = string.split(':')[1]
 
 	try:
-		before, after = check_host(host, port)
+		before, after, key_security = check_host(host, port)
 	except Exception:
 		print(f"-> Failed connecting to {host} on port {port}")
 		results.append(
-			[host, port, "0", "0", "Failed to connect"]
+			[host, port, "?", "0", "0", "Failed to connect"]
 		)
 		return
 
 	d_before, d_after = decode_result(before, after)
 	str_before, str_after = string_date(d_before), string_date(d_after)
 
-	print(f"-> Result: {str_before}, {str_after}")
+	print(f"-> Result: {str_before}, {str_after}, {key_security}")
 
 	results.append(
-		[host, port, str_before, str_after, "Success"]
+		[host, port, key_security, str_before, str_after, "Success"]
 	)
 
 
@@ -64,14 +64,18 @@ def check_host(hostname, port):
 	cert = ssl.get_server_certificate((hostname, port))
 
 	x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-	print(get_dns_names(x509))
+	# print(get_dns_names(x509))
 	print(str(x509.get_signature_algorithm()))
 	print(str(x509.get_issuer()))
+
+	key_type = 'RSA' if x509.get_pubkey().type() == OpenSSL.crypto.TYPE_RSA else 'DSA'
+	key_length = x509.get_pubkey().bits()
+	key_security = f"{key_type}-{key_length}"
 
 	before = x509.get_notBefore()
 	after = x509.get_notAfter()
 
-	return before, after
+	return before, after, key_security
 
 
 def get_dns_names(req: OpenSSL.crypto.X509):
